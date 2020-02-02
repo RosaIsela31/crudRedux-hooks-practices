376a2510180b76d9c6d16402eea79a489f20c28c
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-02-02 13:12:31 -0600

    config react with redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Products from './components/Products';
+import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
+
+import './index.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className='container mt-5'>
+        <Switch>
+          <Route exact path='/' component={Products} />
+          <Route exact path='/productos/nuevo' component={NuevoProducto} />
+          <Route exact path='/productos/editar/:id' component={EditarProducto} />
+
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+// import React, { Fragment } from 'react';
+// import Header from './Header';
+// import Products from './Products';
+
+// const Container = () => {
+//   return ( 
+//     <Fragment>
+//       <Header /> 
+//     </Fragment>
+//    );
+// }
+ 
+// export default Container;
